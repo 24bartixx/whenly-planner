7ae0d01304fc2cd305d7168b09c9e97754c50585
24bartixx <jestgitbartix@gmail.com>
2025-11-01 23:31:05 +0100

    feat: task model mocks

--- lib/features/todo/presentation/widgets/sliver_tasks_section.dart
+++ lib/features/todo/presentation/widgets/sliver_tasks_section.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:whenly_planner/config/ui_config.dart';
+import 'package:whenly_planner/features/todo/data/models/task.dart';
+import 'package:whenly_planner/features/todo/data/models/task_priority.dart';
 import 'package:whenly_planner/features/todo/presentation/widgets/task_tile.dart';
 
 class SliverTasksSection extends StatelessWidget {
-  SliverTasksSection({super.key});
-
-  final mockTasks = [
-    {"title": "Task 1"},
-    {"title": "Task 2"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-    {"title": "Task 3"},
-  ];
+  const SliverTasksSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Task> mockTasks = List.generate(10, (i) {
+      final now = DateTime.now();
+      final priorities = <TaskPriority>[
+        TaskPriority.low,
+        TaskPriority.medium,
+        TaskPriority.high,
+      ];
+
+      return Task(
+        title: 'Task #${i + 1}',
+        done: i.isEven,
+        priority: priorities[i % priorities.length],
+        taskDdl: now.add(Duration(days: i + 1)),
+        createdAt: now.subtract(Duration(hours: i * 6)),
+      );
+    });
+
     return SliverPadding(
       padding: const EdgeInsets.all(AppPaddings.large),
       sliver: SliverList.builder(
